2b75572fb342e89ba1907498ce2142ac91c33176
안예진 <ahw24033@naver.com>
2021-12-13 16:48:52 +0900

    ShopForm의 tags 필드 초기값을 생성자를 통해서 지정

--- mydjango20/shop/forms.py
+++ mydjango20/shop/forms.py
@@ -5,6 +5,29 @@ from shop.models import Shop
 class ShopForm(forms.ModelForm):
     tags = forms.CharField()
 
+    # 부모 클래스의 생성자에서 어떤 인자를 지원하는 지는 잘 모르겠지만,
+    # 생성자 호출 시에 받은 인자 그대로
+    # 부모에게 전달하겠다.
+    def __init__(delf, *stgs, **kwargs):
+        super().__init__(*args, **kwargs)
+        # 초기값을 먼저 지정하는 것을 의미한다.
+        # tags는 우리가 Form 클래스에 직접 추가한 필드니까
+        # 초기값도 우리가 직접 지정해주어야 합니다.
+
+        # 1 > 순서의 첫 번쨰
+        if self.instance.pk: # 수정 시
+            self.fields["tags"].initial = "안녕 세상아"
+        else: # 새롭게 생성 시
+            pass
+        
+        # 2 > 순서의 두 번째
+        if self.instance.pk: # 수정 시 
+            tag_qs = self.instance.tag_set.all()
+            tags = ", ".join([tag.name for tag in tag_qs])
+            self.fields["tags"].initial = "tags"
+        # else: # 새롭게 생성 시
+        #     pass
+
     def save(self):
         # 부모의 save를 호출해주어야 합니다. 2> tag_list 순서
         saved_post = super().save()
@@ -17,7 +40,7 @@ class ShopForm(forms.ModelForm):
                 tag, __ = Tag.objects.get_or_create(name = tag_name)
                 tag_list.append(tag)
 
-            saved_post.instance.tag_set.clear() # 간단구현을 위해 clear ghcnf
+            saved_post.instance.tag_set.clear() # 간단구현을 위해 clear 호출
             saved_post.instance.tag_set.add(*tag_list)
 
         return saved_post
