c78d6e4fbb451d5582b0148c0745190f105706fa
안예진 <ahw24033@naver.com>
2021-12-14 16:00:01 +0900

    갈길을 잃어버린 코드

--- mydjango21/shop/urls.py
+++ mydjango21/shop/urls.py
@@ -4,6 +4,8 @@ from django.urls import path
 app_name = "shop"
 
 urlpatterns = [
+    path('', views.shop_list, name = "shop_list"),
     path('<int:pk>', views.shop_new, name="shop_new"),
     path('show/',views.show_form, name='show_form'),
+    path("<int:pk>/edit/",views.shop_edit, name="shop_edit"),
     ]
