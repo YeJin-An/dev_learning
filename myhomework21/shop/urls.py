d146011c173c01d871a85edadbb4ebd97ac51b1a
안예진 <ahw24033@naver.com>
2021-12-15 18:26:28 +0900

    file 정리

--- myhomework21/shop/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path
-
-app_name = 'shop'
-
-urlpatterns = [
-    path("<int:pk>/",views.shop_detail, name = "shop_detail"),
-    path('new/',views.shop_new, name = "shop_new"),
-]
