8c48855121eddb88c1e506cbb63b1d6fe3d64443
안예진 <ahw24033@naver.com>
2021-12-15 10:39:17 +0900

    django generic user

--- mydjango23/mydjango23/urls.py
+++ mydjango23/mydjango23/urls.py
@@ -8,10 +8,13 @@ from django.conf.urls.static import static
 from mydjango23.settings import DEBUG
 
 from django.shortcuts import redirect
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
+    # FIXME: 아래의 url 설정은 blog 기능 구현 후에 pattern_name 으로 변경 예정
+    path('', RedirectView.as_view(url='/blog/'), name = 'root'),
 ]
 
 # static의 반환값은 리스트!!
