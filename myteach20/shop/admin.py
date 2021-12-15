b81888e35a7ca63a5f2bcf23c11d9dae6c6bbe41
안예진 <ahw24033@naver.com>
2021-12-15 09:29:19 +0900

    teach code

---
+++ myteach20/shop/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+
+from shop.forms import ShopForm
+from shop.models import Category, Shop, Review, Tag
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    form = ShopForm
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
