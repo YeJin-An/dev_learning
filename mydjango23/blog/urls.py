7b78899c855819d6d39bedadcc040074bf6c017c
안예진 <ahw24033@naver.com>
2021-12-15 12:34:11 +0900

    view 기본 설정

--- mydjango23/blog/urls.py
+++ mydjango23/blog/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 app_name = 'blog'
 
-urlpatterns = []
+urlpatterns = [
+    path('', views.post_list, name = "post_list"),
+    path('<int:pk>/', views.post_detail, name = "post_detail"),
+    path('new/', views.post_new, name = "post_new"),
+    path('<int:pk>/edit', views.post_edit, name = "post_edit"),
+    path('<int:pk>/delete/', views.post_delete, name = "post_delete"),
+]
--- mydjango23/blog/views.py
+++ mydjango23/blog/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect, get_object_or_404
+from django.http import HttpRequest, HttpResponse
+from django.contrib.messages import messages
 
-# Create your views here.
+from blog.models import Post
+from blog.forms import PostForm
+
+
+def post_list(request:HttpRequest)-> HttpResponse:
+    post_qs = Post.objects.all()
+    # render 파일을 읽고 뭉쳐주는 역활
+    return render(request, 'blog/post_list.html',{
+        'post_list':post_qs, # 참조를 하겠다.
+    })
+
+def post_detail(request:HttpRequest, pk:int)-> HttpResponse:
+    post = post_get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html',{
+
+    })
+
+def post_new(request:HttpRequest) ->HttpResponse:
+    pass
+
+def post_edit(request:HttpRequest, pk:int)->HttpResponse:
+    pass
+
+def post_delete(request:HttpRequest, pk:int)->HttpResponse:
+    pass
