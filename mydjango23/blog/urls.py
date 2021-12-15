87ce367f52a142cbf32816f8647855e34ed523d7
안예진 <ahw24033@naver.com>
2021-12-15 15:28:26 +0900

    ListView 오타 수정 (차례이해)

--- mydjango23/blog/urls.py
+++ mydjango23/blog/urls.py
@@ -1,12 +1,15 @@
 
 from django.urls import path
 
+from blog.views import fbv as views
+# from blog.views import cbv as views
+
 app_name = "blog"
 
 urlpatterns = [
-    path("", post_list, name="post_list"),
-    path("<int:pk>/", post_detail, name="post_detail"),
-    path("new/", post_new, name="post_new"),
-    path("<int:pk>/edit/", post_edit, name="post_edit"),
-    path("<int:pk>/delete/", post_delete, name="post_delete"),
+    path("", views.post_list, name="post_list"),
+    path("<int:pk>/", views.post_detail, name="post_detail"),
+    path("new/", views.post_new, name="post_new"),
+    path("<int:pk>/edit/", views.post_edit, name="post_edit"),
+    path("<int:pk>/delete/", views.post_delete, name="post_delete"),
 ]
--- mydjango23/blog/view/cbv.py
+++ mydjango23/blog/view/cbv.py
@@ -18,14 +18,14 @@ class PostCreateView(CreateView):
     form_class = PostForm
     # success_url = reverse_lazy("blog:post_list")
 
-    def get_success_url(self):
-        # self.object # 저장된 모델 인스턴스
-        post_pk = self.object.pk
-        ## 이것은 템플릿을 사용할때 사용하는 방법이다.
-        return reverse('blog:post_detail', args = [post_pk]) # 문자열 [낮은 단계의 기본]
-        #return resolve_url("blog:post_detail" , post_pk)     # 문자열 [ 기능이 보다 많다. ]
-        #return redirect("blog:post_detail", post_pk)         # HttpResponse
-        #           {% url "blog:post_detail" post_pk %}     # 문자열
+    # def get_success_url(self):
+    #     # self.object # 저장된 모델 인스턴스
+    #     post_pk = self.object.pk
+    #     ## 이것은 템플릿을 사용할때 사용하는 방법이다.
+    #     return reverse('blog:post_detail', args = [post_pk]) # 문자열 [낮은 단계의 기본]
+    #     #return resolve_url("blog:post_detail" , post_pk)     # 문자열 [ 기능이 보다 많다. ]
+    #     #return redirect("blog:post_detail", post_pk)         # HttpResponse
+    #     #           {% url "blog:post_detail" post_pk %}     # 문자열
 
 post_new = PostCreateView.as_view()
 
