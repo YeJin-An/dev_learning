95d12e7fbc719481896a9874c688c3e7dc795770
안예진 <ahw24033@naver.com>
2021-12-15 16:19:30 +0900

    accounts apps crate

--- mydjango23/mydjango23/urls.py
+++ mydjango23/mydjango23/urls.py
@@ -13,6 +13,7 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
+    path('accounts/', includ('accounts.urls')),
     # FIXME: 아래의 url 설정은 blog 기능 구현 후에 pattern_name 으로 변경 예정
     path('', RedirectView.as_view(url='/blog/'), name = 'root'),
 ]
