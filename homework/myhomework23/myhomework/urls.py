1759642e7f8214f9f2c5032aaa4ec1afa2ba0188
안예진 <ahw24033@naver.com>
2021-12-15 18:50:26 +0900

    django settings

---
+++ homework/myhomework23/myhomework/urls.py
@@ -0,0 +1,21 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
+from django.conf import settings
+from django.conf.static import static
+
+from myhomerk.settings import DEBUG
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+urlpatterns += static(settings.MEDIA_URL,
+                        document_root = settings.MEDIA_ROOT)
+
+if DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
