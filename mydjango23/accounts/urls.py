6b6b319b64fb96e2b7e7e8d9316d9f295991672c
안예진 <ahw24033@naver.com>
2021-12-15 16:50:55 +0900

    admin 의 회원가입 기능

--- mydjango23/accounts/urls.py
+++ mydjango23/accounts/urls.py
@@ -1,3 +1,11 @@
+from django.urls import path
+from accoints import views
+
 app_name = "account"
 
-urlpatterns = []
+urlpatterns = [
+    path('login/', views.login, name = 'login'),
+    path('signip/', views.signup, name = "signup"),
+    path('profile/', views.profile, name = "profile"),
+    path('logout/', views.logout, name = "logout"),
+]
