ba81b0a2b290d1b6812ce85e4ec997d31e7d3bdf
안예진 <ahw24033@naver.com>
2021-12-16 18:34:47 +0900

    app_YeJin settings

--- homework/myhomework23/web/models.py
+++ homework/myhomework23/web/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class TimestampledModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(suto_now = True)
+
+class Category(models.Model):
+    pass
+
+class Post(models.Model):
+    pass
+
+class Comment(models.Model):
+    pass
+
+class Tag(models.Model):
+    pass
