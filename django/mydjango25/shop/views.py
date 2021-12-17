c8c4261ad798b361d60ee448fe2c36f7a3e80039
안예진 <ahw24033@naver.com>
2021-12-17 15:24:35 +0900

    back __ i don't know idea

--- django/mydjango25/shop/views.py
+++ django/mydjango25/shop/views.py
@@ -10,3 +10,29 @@ class ShopListView(ListView):
         return context_data
 
 shop_list = ShopListView.as_view()
+
+shop_datail = DetailView.as_view(
+    model = Shop,
+)
+
+class ReviewCreateView(CreateView):
+    model = Review
+    form_class = ReviewForm
+    success_url = reverse_lazy("shop:shop_list")
+
+    # 유효성 검사에 통과한다면
+    def form_valid(self, form)->HttpResponse:
+        # self.kwarg : URL Captured 값들이 사전으로 저장
+        shop = get_object_or_404(Shop, pk=shop_pk)
+
+        self.kwargs["shop_pk"]
+        review = form.save(commit=False)
+        review.shop = shop
+        review.user = self.request.user
+        review.save()
+        
+        # return. redirect ("shop:shop_detail", shop.pk)
+        return redirect(shop)
+
+# TODO: shop detail 로 이동
+review_new = CreateView.as_view()
