b0f3a73c8bbb9310eaf16bc4ee676f7bf229f2c8
안예진 <ahw24033@naver.com>
2021-12-17 10:45:58 +0900

    웹 페이지 파일경로 지정

--- django/mydjango25/mydjango25/urls.py
+++ django/mydjango25/mydjango25/urls.py
@@ -1,6 +1,7 @@
 
 from django.contrib import admin
 from django.urls import path , include
+from django.views.generic import TemplateView
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -10,6 +11,7 @@ from mydjango25.settings import DEBUG
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("shop/", include('shop.urls')),
+    path('',TemplateView.as_view(template_name = 'root.html'),name = 'root'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,
