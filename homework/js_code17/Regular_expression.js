1d26e4f8472d57bd96ad54da9175be0d550d42c4
안예진 <ahw24033@naver.com>
2021-12-21 21:58:45 +0900

    4 class startup

--- homework/js_code17/Regular_expression.js
+++ homework/js_code17/Regular_expression.js
@@ -15,13 +15,43 @@ var pattern = /a/ // 리터럴
 var pattern = new RegExp('a'); // 객체 생성자
 
 // regular_expression 메소드 실행
-// __ 원하는 문자 찾기 [exec, test]
-console.log(pattern.exec('abcdef'));
-console.log(pattern.exec('bcdefg'));
+// __ 원하는 문자 찾기 [ exec, test ]
+console.log(pattern.exec('abcdef')); //["a"]
+console.log(pattern.exec('bcdefg')); // null
 
-console.log(pattern.test('abcdef'));
-console.log(pattern.test('bcdefg'))
+console.log(pattern.test('abcdef')); // true
+console.log(pattern.test('bcdefg')); // false
 
+
+// 문자열 메소드 실행 [ match, replace ]
+console.log('abcdef'.match(pattern)); // ["a"]
+console.log('bcdefg'.match(pattern)); // null
+
+console.log('abcdef'.replace(pattern,"A")); //Abcdef
+
+
+// option [i, g]
+var xi = /a/;
+console.log("Abcde".match(xi)); //null
+var oi = /a/i;
+console.log("Abcde"/match(oi)); //["A"];
+// 대소문자를 구분하지 않는다.
+
+var xg = /a/;
+console.log("abcdea".match(xg));
+var og = /a/g;
+console.log("abcdea"/match(og));
+// 검색된 모든 결과를 리턴 
+
+
+// 활용 사례__ 치환
+// [ 아래 코드는 본문 중의 URL을 링크 html태그로 교체 ]
+var urlPattern = /\b(?:https?):\/\/[a-z0-9-+&@#\/%?=~_|!:,.;]*/gim;
+var content = '여기는 ... 입니다. 네이버 : http://naver.com 입니다. ';
+var result = content.replace(urlPattern, function(url){
+    return '<a href="'+url+'">'+url+'</a>';
+});
+console.log(result);
 
 
 
