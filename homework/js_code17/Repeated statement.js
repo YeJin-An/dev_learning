06a15ae8f5f4cacfb2316fe5d3f8aac98842dd15
안예진 <ahw24033@naver.com>
2021-12-21 19:01:53 +0900

    2 class

--- homework/js_code17/Repeated statement.js
+++ homework/js_code17/Repeated statement.js
@@ -1,11 +1,49 @@
+/*
+저장되지 않는 작업이 있다면, 정리 후 명령 실행
+웹브라우저는 무한반복을 허용안해 스크립트 종료를 물어봄
+
+document.write -> 자바스크립트로 웹페이지에 텍스트 출력
+node.js 콘솔과 같은 환경에서 실습하면, console.log로 사용
+*/
+
+while(true){
+    document.write("coding everybody <br />");}
+while(false){
+    document.write("coding everybody <br />");}
+
+// while
+var i =0;
+while(i < 10){
+    document.write("coding everybody <br />");
+    i++} // coding everybody <br /> 10번
+
+// for
+for(var i =0; i <10; i++){
+    document.write("coding everybody"+i+"<br />");
+} // coding everybody i <br />
+
+// break
+for(var i=0; i<10; i++){
+    if(i === 5){
+        break;
+    }
+    document.write("coding everybody" + i+ "<br />");
+ } // coding everybody 5개 <br />
 
 
-while (조건){
-    반복해서 실행할 코드}
+// continue
+for(var i = 0; i < 10; i++){
+    if(i === 5){
+        continue;
+    }
+    document.write("coding everybody"+i+'<br />');
+} // coding everybody 10개 <br />
 
-// 저장되지 않는 작업이 있다면, 정리 후 명령 실행
-// 웹브라우저는 무한반복을 허용안해 스크립트 종료를 물어봄
 
-// document.write -> 자바스크립트로 웹페이지에 텍스트 출력
-// node.js 콘솔과 같은 환경에서 실습하면, console.log로 사용
-
+// break && continue
+for(var i=0; i<10; i++){
+    for (var j=0; j<10; j++){
+        document.write(String(i)+String(j)+'<br />');
+    }
+} // n -> 10개  ==> n * 10 = 100번 출력
+  //    00 ~ 99 ==> 100개 출력
---
+++ homework/js_code17/function.js
@@ -0,0 +1,20 @@
+/*
+function 함수명 ( [인지 ... [,인자]] ){
+    코드
+    return 반환값}
+*/
+
+// 함수의 정의와 호출
+function numbering(){
+    i = 0;
+    while(i< 10){
+        document.write(i);
+        i += 1;
+    }
+}
+numbering();
+// 0123456789
+
+
+// 함수가 없다면, 반복문을 n번 반복 해야한다.
+
